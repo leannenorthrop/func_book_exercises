dd1dd7ededf41236557417b1db24592dc775cb25
Leanne Northrop <leanne.northrop@googlemail.com>
2015-07-03 17:54:06 +0100

    Preparing for Chapter 3 exercises and examples.

---
+++ FuncProgramming/Chp3.swift
@@ -0,0 +1,9 @@
+//
+//  Chp3.swift
+//  FuncProgramming
+//
+//  Created by Leanne Northrop on 03/07/2015.
+//  Copyright (c) 2015 Leanne Northrop. All rights reserved.
+//
+
+import Foundation
---
+++ FuncProgrammingTests/Chp3Tests.swift
@@ -0,0 +1,18 @@
+//
+//  Chp3Tests.swift
+//  FuncProgramming
+//
+//  Created by Leanne Northrop on 03/07/2015.
+//  Copyright (c) 2015 Leanne Northrop. All rights reserved.
+//
+
+import XCTest
+
+class Chp3Tests: XCTestCase {
+
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+
+}
