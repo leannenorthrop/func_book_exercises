4d5b0b14c570e15eb1e1968b4f765357d63edc3d
Leanne Northrop <leanne.northrop@googlemail.com>
2015-07-03 15:26:44 +0100

    Implemented exercise 2.1 fibonacci numbers.

--- FuncProgramming/Chp2.swift
+++ FuncProgramming/Chp2.swift
@@ -18,3 +18,45 @@ func factorial(n:Int) -> Int {
     }
     return loop(n,1)
 }
+
+// Exercise 1.2
+/// Computes Fibonacci number 
+func fib(n:Int) -> Int {
+    var loop: (Int) -> Int = { _ in return 0}
+    loop = {
+        n in
+        return n <= 0 ? 0 : n == 1 ? 1 : loop(n-2)+loop(n-1)
+    }
+    return loop(n-1)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//*****************************************************************************************
+//    Playground
+//*****************************************************************************************
+/// Being curious about for loops
+/// :param: Number of duplicates
+/// :param: First string
+/// :param: Second string
+/// :results: First and second string duplicated
+func duplicateStrings(n:Int,s1:String,s2:String) -> (String,String) {
+    var loop: (Int,String,String) -> (String,String) = {_,_,_ in return ("","")}
+    loop = {
+        n,str1,str2 in
+        // current state of loop is captured in parameters
+        // loop counts down instead of imperative for loop which counts up
+        return n <= 1 ? (str1,str2) : loop(n-1,s1+str1,s2+str2)
+    }
+    return loop(n,s1,s2)
+}
--- FuncProgrammingTests/Chp2Tests.swift
+++ FuncProgrammingTests/Chp2Tests.swift
@@ -17,5 +17,24 @@ class Chp2Tests: XCTestCase {
     func testFactorial() {
         XCTAssertEqual(6, factorial(3))
     }
-
+    
+    func testFibEndCase() {
+        XCTAssertEqual(0, fib(0))
+    }
+    
+    func testFib() {
+        XCTAssertEqual(0, fib(1))
+        XCTAssertEqual(1, fib(2))
+        XCTAssertEqual(1, fib(3))
+        XCTAssertEqual(2, fib(4))
+        XCTAssertEqual(3, fib(5))
+        XCTAssertEqual(5, fib(6))
+        XCTAssertEqual(8, fib(7))
+    }
+    
+    func testDuplicateStrings() {
+        let result : (one:String,two:String) = duplicateStrings(3, "hi", "bye")
+        XCTAssertEqual("hihihi", result.one)
+        XCTAssertEqual("byebyebye", result.two)
+    }
 }
