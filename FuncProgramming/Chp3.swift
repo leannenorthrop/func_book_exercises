6dd74026136ae429a3f0080a687fe7d728b5b54b
Leanne Northrop <leanne.northrop@googlemail.com>
2015-07-03 19:35:42 +0100

    Implemented listing 3.1

--- FuncProgramming/Chp3.swift
+++ FuncProgramming/Chp3.swift
@@ -8,17 +8,95 @@
 
 import Foundation
 
+//***************************************************************************************
+// Listing 3.1
 class List<A> {
-    let list:[A]
+    let head:A?
+    let tail:List<A>?
+    
     var isEmpty : Bool {
-        return self.list.isEmpty
+        return self.head == nil && self.tail == nil
     }
     
     init() {
-        list = []
+        self.head = nil
+        self.tail = nil
+    }
+
+    init(head:A, tail:List<A>?){
+        self.head = head
+        self.tail = tail
     }
     
-    init(a:A...){
-        list = Array(a)
+    init(arr:[A]){
+        var a = arr
+        let head = a.removeAtIndex(0)
+        var tail : [A] = a
+        
+        self.head = head
+        self.tail = tail.isEmpty ? nil : List<A>(arr:tail)
+    }
+    
+    init(arr:A...){
+        var a = arr
+        let head = a.removeAtIndex(0)
+        var tail : [A] = a
+        
+        self.head = head
+        self.tail = tail.isEmpty ? nil : List<A>(arr:tail)
+    }
+    
+    subscript(index: Int) -> A? {
+        var loop : Int -> A? = { _ in return nil }
+        loop = {
+            idx in
+            if self.isEmpty {
+                return nil
+            } else {
+                return idx == 0 ? self.head : self.tail![idx-1]
+            }
+        }
+        return loop(index)
     }
 }
+
+struct ListHelpers {
+    func sum(ints:List<Int>?) -> Int {
+        if ints == nil {
+            return 0
+        } else {
+            var lst = ints!
+            return lst.tail == nil ? lst.head! : lst.head! + sum(lst.tail)
+        }
+    }
+    
+    func apply<A>(arr:[A]) -> List<A> {
+        var a = arr
+        if a.isEmpty {
+            return List<A>()
+        } else {
+            let head = arr[0]
+            var tail:[A]
+            switch arr.count {
+            case 1 : tail = []
+            case 2 : tail = [arr[1]]
+            default : tail = Array(arr[1..<arr.count])
+            }
+
+            return List<A>(head:head, tail: apply(tail))
+        }
+    }
+    
+    func product(ds:List<Float>?) -> Float {
+        if ds == nil {
+            return Float(1.0)
+        } else if ds!.isEmpty {
+            return Float(1.0)
+        } else if ds!.head == Float(0.0) {
+            return Float(0.0)
+        } else {
+            return ds!.head! * product(ds!.tail)
+        }
+    }
+}
+//***************************************************************************************
--- FuncProgrammingTests/Chp3Tests.swift
+++ FuncProgrammingTests/Chp3Tests.swift
@@ -13,4 +13,33 @@ class Chp3Tests: XCTestCase {
     func testEmptyList() {
         XCTAssertTrue(List<Int>().isEmpty)
     }
+    
+    func testList() {
+        XCTAssertFalse(List<Int>(arr:1,2,3).isEmpty)
+    }
+    
+    func testSubscript() {
+        var lst = List<String>(arr:"a","b","c","d","e")
+        XCTAssertEqual("a", lst[0]!)
+        XCTAssertEqual("c", lst[2]!)
+        XCTAssertEqual("d", lst[3]!)
+    }
+    
+    func testSum() {
+        var lst = List<Int>(arr:1,2,3,5)
+        XCTAssertEqual(11, ListHelpers().sum(lst))
+    }
+    
+    func testApply() {
+        var lst : List<String> = ListHelpers().apply(["a","b","c","d","e"])
+        XCTAssertEqual("a", lst[0]!)
+        XCTAssertEqual("c", lst[2]!)
+        XCTAssertEqual("d", lst[3]!)
+        XCTAssertEqual("e", lst[4]!)
+    }
+    
+    func testProduct() {
+        var lst = List<Float>(arr:1,2,3,5)
+        XCTAssertEqual(30, ListHelpers().product(lst))
+    }
 }
