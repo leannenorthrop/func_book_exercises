4a6642b91c58fe9471247983e2a8d4d4a392379b
Leanne Northrop <leanne.northrop@googlemail.com>
2015-07-05 15:37:33 +0100

    Implemented exercises up to 3.6

--- FuncProgramming/Chp3.swift
+++ FuncProgramming/Chp3.swift
@@ -3,16 +3,29 @@
 //  FuncProgramming
 //
 //  Created by Leanne Northrop on 03/07/2015.
-//  Copyright (c) 2015 Leanne Northrop. All rights reserved.
 //
+//  Interesting discussion on currying
+// http://programmers.stackexchange.com/questions/185585/what-is-the-advantage-of-currying
 
 import Foundation
 
 //***************************************************************************************
 // Listing 3.1
-class List<A> {
+class List<A> : Printable {
     let head:A?
     let tail:List<A>?
+    var description :String {
+        var loop : List<A>->String = { _ in return ""}
+        loop = {
+            lst in
+            var str = "unknown, "
+            if let v = lst.head as? Printable {
+                str = v.description + ", "
+            }
+            return lst.isEmpty ? "" : str + loop(lst.tail!)
+        }
+        return loop(self)
+    }
     
     var isEmpty : Bool {
         return self.head == nil && self.tail == nil
@@ -67,7 +80,6 @@ struct ListHelpers {
         let (head,tail) = ints!.tuple()
         switch (head,tail) {
         case(nil,nil): return 0
-        case(let head,nil) : return head!
         case(let head, let tail) : return head! + sum(tail)
         }
     }
@@ -93,9 +105,13 @@ struct ListHelpers {
         let (head,tail) = floats!.tuple()
         switch (head,tail) {
         case(nil,nil): return Float(1.0)
-        case(let head,nil) : return head!
         case(let head, let tail) : return head! * product(tail)
         }
+    }
+    
+    /// Exercise 3.4 Drop the first n head items from the list
+    func drop<A>(l:List<A>, n:Int) -> List<A> {
+        return n == 0 || l.isEmpty ? l : drop(l.tail!,n:n-1)
     }
     
     /// Exercise 3.5 Drop the head items from the list while f returns true
@@ -104,5 +120,21 @@ struct ListHelpers {
         return l.isEmpty ? l : (f(l.head!)  ? drop(l.tail!, f:f) : l)
     }
     
+    /// Exercise 3.6
+    func head2<A>(l:List<A>) -> List<A> {
+        let (head,tail) = l.tuple()
+        return l.isEmpty ? List<A>() : (tail!.isEmpty ? List<A>() : List<A>(head:head!, tail:head2(tail!)))
+    }
+    
+    /// Curried drop  the head items from the list while f returns true
+    /// :returns: New list
+    func drop<A>(l: List<A>)(f: A -> Bool) -> List<A> {
+        if f(l.head!) {
+            return drop(l.tail!)(f:f)
+        } else {
+            return l
+        }
+    }
+
 }
 //***************************************************************************************
--- FuncProgrammingTests/Chp3Tests.swift
+++ FuncProgrammingTests/Chp3Tests.swift
@@ -20,6 +20,11 @@ class Chp3Tests: XCTestCase {
         XCTAssertFalse(lst.isEmpty)
     }
     
+    func testPrintable() {
+        let lst = List<Int>(head:1, tail:List<Int>(head:2,tail:List<Int>(head:3)))
+        XCTAssertEqual("1, 2, 3, ", lst.description)
+    }
+    
     func testSum() {
         var lst : List<Int> = listOps.apply([1,2,3,5])
         XCTAssertEqual(11, listOps.sum(lst))
@@ -38,7 +43,18 @@ class Chp3Tests: XCTestCase {
         XCTAssertEqual(30, listOps.product(lst))
     }
     
-    func testTail() {
+    // Not sure of the relevance of currying drop as 
+    // type inference in swift seems to handle closure types
+    // perfectly well as seen in Exercise 3.5 below
+    func testCurriedDrop() {
+        var lst : List<Int> = listOps.apply([1,3,5, 7,8,52,5,7])
+        lst = listOps.drop(lst)(f:{ $0 < 10 })
+        XCTAssertEqual(52, lst[0]!)
+        XCTAssertEqual(5, lst[1]!)
+        XCTAssertEqual(7, lst[2]!)
+    }
+    
+    func testExercise3_2() {
         var lst : List<String> = listOps.apply(["a","b","c","d","e"])
         lst = lst.tail2()!
         XCTAssertEqual("b", lst[0]!)
@@ -47,7 +63,7 @@ class Chp3Tests: XCTestCase {
         XCTAssertEqual("e", lst[3]!)
     }
     
-    func testReplaceFirst() {
+    func testExercise3_3() {
         var lst : List<String> = listOps.apply(["a","b","c","d","e"])
         lst = lst.setHead("z")
         XCTAssertEqual("z", lst[0]!)
@@ -56,12 +72,29 @@ class Chp3Tests: XCTestCase {
         XCTAssertEqual("d", lst[3]!)
         XCTAssertEqual("e", lst[4]!)
     }
+
+    func testExercise3_4() {
+        var lst : List<Int> = listOps.apply([1,3,5,7,8,52,5,7])
+        lst = listOps.drop(lst,n:3)
+        XCTAssertEqual(7, lst[0]!)
+        XCTAssertEqual(8, lst[1]!)
+        XCTAssertEqual(52, lst[2]!)
+    }
     
-    func testDrop() {
+    func testExercise3_5() {
         var lst : List<Int> = listOps.apply([1,3,5, 7,8,52,5,7])
         lst = listOps.drop(lst) { $0 < 10 }
         XCTAssertEqual(52, lst[0]!)
         XCTAssertEqual(5, lst[1]!)
         XCTAssertEqual(7, lst[2]!)
+    }
+    
+    func testExercise3_6() {
+        var lst : List<Int> = listOps.apply([1,3,8,52])
+        let result1 = listOps.head2(lst)
+        XCTAssertEqual(1, result1[0]!)
+        XCTAssertEqual(3, result1[1]!)
+        XCTAssertEqual(8, result1[2]!)
+        XCTAssertNil(result1[3])
     }
 }
